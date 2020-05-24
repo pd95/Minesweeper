27dd5949b9c8fdbe48eeb59031d5f9571156c23b
Philipp <pd95@users.noreply.github.com>
2020-05-24 23:39:42 +0200

    A first try at implementing Minesweeper for iPhone

--- Minesweeper/ContentView.swift
+++ Minesweeper/ContentView.swift
@@ -9,8 +9,115 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var feedback = UINotificationFeedbackGenerator()
+
+    @ObservedObject var game = MinesweeperGame()
+
+    var gameState: String {
+        switch game.state {
+            case .running:
+                return "Can you locate the \(game.numberOfMines) mines?"
+            case .lost:
+                return "The game is over. You have lost!"
+            case .won:
+                return "Well done! You win!"
+        }
+    }
+
+    var buttonText: String {
+        if game.state == .won {
+            return "Next level"
+        }
+        return "Restart"
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 10) {
+            Text("Mine Sweeper")
+                .font(.largeTitle)
+
+            Text(gameState)
+
+            ZStack {
+                VStack(spacing: 0) {
+                    ForEach(0..<game.height, id: \.self) { row in
+                        HStack(spacing: 0) {
+                            ForEach(0..<self.game.width, id: \.self) { column in
+                                self.cellView(for: row, column: column)
+                            }
+                        }
+                    }
+                }
+
+                Button(action: restart) {
+                    Text(buttonText)
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.red)
+                            .shadow(radius: 10)
+                        )
+                        .foregroundColor(Color.white)
+                }
+                .frame(maxWidth: 300, maxHeight: 300)
+                .background(Color.white.opacity(0.3))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .opacity(game.state != .running ? 1 : 0)
+                .animation(Animation.easeIn)
+            }
+            Spacer()
+        }
+        .padding()
+    }
+
+    func restart() {
+        var numberOfMines = self.game.numberOfMines
+        if self.game.state == .won {
+            numberOfMines += 5
+        }
+        self.game.reset(numberOfMines: numberOfMines)
+    }
+
+    func cellView(for row: Int, column: Int) -> some View {
+        let state = game.field[row][column]
+        let content: String
+        switch state {
+            case .outOfBounds:
+                content = "?"
+            case .covered:
+                content = " "
+            case .explodedMine:
+                content = "💥"
+            case .hiddenMine:
+                content = game.state == .lost ? "💣" : " "
+            case .uncovered(numberOfNeighbouringMines: let numberOfNeighbouringMines):
+                content = numberOfNeighbouringMines > 0 ? "\(numberOfNeighbouringMines)" : " "
+            case .flagged:
+                content = "🏴"
+            case .flaggedMine:
+                content = game.state == .lost ? "💣" : "🏴"
+        }
+        return Text("xx")
+            .hidden()
+            .overlay(
+                Text(content)
+            )
+            .background(state.isUncovered ? Color.clear : Color.yellow)
+            .overlay(
+                Rectangle()
+                    .stroke()
+            )
+            .font(Font.title.bold())
+            .onTapGesture {
+                let field = self.game.uncover(location: (row, column))
+                if field == .explodedMine {
+                    self.feedback.notificationOccurred(.error)
+                }
+            }
+            .onLongPressGesture() {
+                if self.game.flagMine(at: (row, column)) {
+                    self.feedback.notificationOccurred(.success)
+                }
+            }
     }
 }
 
---
+++ Minesweeper/MinesweeperGame.swift
@@ -0,0 +1,190 @@
+//
+//  MinesweeperGame.swift
+//  Minesweeper
+//
+//  Created by Philipp on 24.05.20.
+//  Copyright © 2020 Philipp. All rights reserved.
+//
+
+import Foundation
+
+class MinesweeperGame: ObservableObject {
+
+    enum FieldState: Hashable, Equatable, CustomStringConvertible {
+        case outOfBounds, covered, hiddenMine, uncovered(numberOfNeighbouringMines: Int), flagged, flaggedMine, explodedMine
+
+        var isUncovered: Bool {
+            return self != .covered && self != .hiddenMine
+        }
+
+        var hasMine: Bool {
+            return self == .flaggedMine || self == .hiddenMine || self == .explodedMine
+        }
+
+        var description: String {
+            switch self {
+                case .outOfBounds:
+                    return "?"
+                case .covered:
+                    return "_"
+                case .explodedMine:
+                    return "💥"
+                case .hiddenMine:
+                    return "💣"
+                case .uncovered(numberOfNeighbouringMines: let numberOfNeighbouringMines):
+                    return numberOfNeighbouringMines > 0 ? "\(numberOfNeighbouringMines)" : " "
+                case .flagged:
+                    return "🏴"
+                case .flaggedMine:
+                    return "🏴‍☠️"
+            }
+        }
+    }
+
+    enum GameState: String {
+        case running, won, lost
+    }
+
+    @Published private(set) var field: [[FieldState]] = []
+    @Published private(set) var state: GameState = .running
+    let width: Int
+    let height: Int
+    var numberOfMines: Int
+    var numberOfFlags = 0
+
+    init(width: Int = 10, height: Int = 15, numberOfMines: Int = 5) {
+        self.width = width
+        self.height = height
+        self.numberOfMines = numberOfMines
+        reset()
+    }
+
+    func reset(numberOfMines: Int? = nil) {
+        let numberOfMines = numberOfMines ?? self.numberOfMines
+        guard numberOfMines > 0 && numberOfMines < self.width * self.height / 4 else {
+            self.state = .lost
+            return
+        }
+        self.numberOfMines = numberOfMines
+
+        var field = [[FieldState]]()
+        (0..<height).forEach { _ in
+            field.append(Array(repeating: FieldState.covered, count: width))
+        }
+        self.field = field
+
+        var minesPlaced = 0
+        while minesPlaced < numberOfMines {
+            let randomLocation = (row: Int.random(in: (0..<height)), column: Int.random(in: (0..<width)))
+
+            if !fieldState(at: randomLocation).hasMine {
+                self.field[randomLocation.row][randomLocation.column] = .hiddenMine
+                minesPlaced += 1
+            }
+        }
+        numberOfFlags = 0
+        self.state = .running
+    }
+
+    func fieldState(at location: (row: Int, column: Int)) -> FieldState {
+        guard (0..<height).contains(location.row) && (0..<width).contains(location.column) else {
+            return .outOfBounds
+        }
+
+        return field[location.row][location.column]
+    }
+
+    func uncover(location: (row: Int, column: Int), initial: Bool = true) -> FieldState {
+        guard state == .running,
+            (0..<height).contains(location.row),
+            (0..<width).contains(location.column) else {
+            return .outOfBounds
+        }
+        let oldState = fieldState(at: location)
+        let state: FieldState
+        if oldState.hasMine {
+            state = .explodedMine
+            self.state = .lost
+        }
+        else {
+            var mineCount = 0
+            for dy in -1...1 {
+                for dx in -1...1 {
+                    mineCount += fieldState(at: (location.row + dy, location.column + dx)).hasMine ? 1 : 0
+                }
+            }
+            state = .uncovered(numberOfNeighbouringMines: mineCount)
+        }
+        field[location.row][location.column] = state
+
+        // Make sure flag counter is decremented if a flag is uncovered
+        if oldState == .flagged {
+            numberOfFlags -= 1
+        }
+
+        if state == .uncovered(numberOfNeighbouringMines: 0) {
+            uncoverNeighbors(location: location)
+        }
+        if initial {
+            updateGameState()
+        }
+
+        return state
+    }
+
+    private func updateGameState() {
+        if state == .running {
+            if numberOfFlags == numberOfMines {
+                for row in field {
+                    for field in row {
+                        if field == .hiddenMine {
+                            return
+                        }
+                    }
+                }
+                state = .won
+            }
+        }
+    }
+
+    private func uncoverNeighbors(location: (row: Int, column: Int)) {
+        for dy in -1...1 {
+            for dx in -1...1 {
+                let newLocation = (location.row + dy, location.column + dx)
+                if fieldState(at: newLocation) == .covered {
+                    _ = uncover(location: newLocation, initial: false)
+                }
+            }
+        }
+    }
+
+    func flagMine(at location: (row: Int, column: Int)) -> Bool {
+        guard state == .running,
+            (0..<height).contains(location.row),
+            (0..<width).contains(location.column) else {
+            return false
+        }
+        let oldNumberOfFlags = numberOfFlags
+        let state = field[location.row][location.column]
+        if state == .covered && numberOfFlags < numberOfMines {
+            field[location.row][location.column] = .flagged
+            numberOfFlags += 1
+        }
+        else if state == .hiddenMine && numberOfFlags < numberOfMines {
+            field[location.row][location.column] = .flaggedMine
+            numberOfFlags += 1
+        }
+        else if state == .flagged {
+            field[location.row][location.column] = .covered
+            numberOfFlags -= 1
+        }
+        else if state == .flaggedMine {
+            field[location.row][location.column] = .hiddenMine
+            numberOfFlags -= 1
+        }
+
+        updateGameState()
+
+        return oldNumberOfFlags < numberOfFlags
+    }
+}
